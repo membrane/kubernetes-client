a607af2404693a47af3b341044f3e1d10fa7bb1f
Niko Ebener <ebener@predic8.de>
2018-02-13 11:58:21 +0100

    avoid duplicate logging

--- src/main/java/de/predic8/kubernetesclient/client/LoggingApiClient.java
+++ src/main/java/de/predic8/kubernetesclient/client/LoggingApiClient.java
@@ -40,137 +40,11 @@ public abstract class LoggingApiClient extends NamespacedApiClient {
         getHttpClient().setReadTimeout(300, TimeUnit.SECONDS);
 
         if (logHttp) {
-            Interceptor hli = new Interceptor() {
-                /*
-                 Source from com.squareup.okhttp:logging-interceptor:2.7.5 , Apache 2 License.
-
-                 Modified to ignore "Upgrade: Websocket" responses.
-                 */
-
-                private final Charset UTF8 = Charset.forName("UTF-8");
-
-                private volatile HttpLoggingInterceptor.Level level = HttpLoggingInterceptor.Level.BODY;
-
-
-                @Override public Response intercept(Chain chain) throws IOException {
-                    HttpLoggingInterceptor.Level level = this.level;
-
-                    Request request = chain.request();
-                    if (level == HttpLoggingInterceptor.Level.NONE) {
-                        return chain.proceed(request);
-                    }
-
-                    boolean logBody = level == HttpLoggingInterceptor.Level.BODY;
-                    boolean logHeaders = logBody || level == HttpLoggingInterceptor.Level.HEADERS;
-
-                    RequestBody requestBody = request.body();
-                    boolean hasRequestBody = requestBody != null;
-
-                    Connection connection = chain.connection();
-                    Protocol protocol = connection != null ? connection.getProtocol() : Protocol.HTTP_1_1;
-                    String requestStartMessage =
-                            "--> " + request.method() + ' ' + request.httpUrl() + ' ' + protocol(protocol);
-                    if (!logHeaders && hasRequestBody) {
-                        requestStartMessage += " (" + requestBody.contentLength() + "-byte body)";
-                    }
-                    LOG.info(requestStartMessage);
-
-                    if (logHeaders) {
-                        if (hasRequestBody) {
-                            // Request body headers are only present when installed as a network interceptor. Force
-                            // them to be included (when available) so there values are known.
-                            if (requestBody.contentType() != null) {
-                                LOG.info("Content-Type: " + requestBody.contentType());
-                            }
-                            if (requestBody.contentLength() != -1) {
-                                LOG.info("Content-Length: " + requestBody.contentLength());
-                            }
-                        }
-
-                        Headers headers = request.headers();
-                        for (int i = 0, count = headers.size(); i < count; i++) {
-                            String name = headers.name(i);
-                            // Skip headers from the request body as they are explicitly logged above.
-                            if (!"Content-Type".equalsIgnoreCase(name) && !"Content-Length".equalsIgnoreCase(name)) {
-                                LOG.info(name + ": " + headers.value(i));
-                            }
-                        }
-
-                        if (!logBody || !hasRequestBody) {
-                            LOG.info("--> END " + request.method());
-                        } else if (bodyEncoded(request.headers())) {
-                            LOG.info("--> END " + request.method() + " (encoded body omitted)");
-                        } else {
-                            Buffer buffer = new Buffer();
-                            requestBody.writeTo(buffer);
-
-                            Charset charset = UTF8;
-                            MediaType contentType = requestBody.contentType();
-                            if (contentType != null) {
-                                contentType.charset(UTF8);
-                            }
-
-                            LOG.info("");
-                            LOG.info(buffer.readString(charset));
-
-                            LOG.info("--> END " + request.method()
-                                    + " (" + requestBody.contentLength() + "-byte body)");
-                        }
-                    }
-
-                    long startNs = System.nanoTime();
-                    Response response = chain.proceed(request);
-                    long tookMs = TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - startNs);
-
-                    ResponseBody responseBody = response.body();
-                    LOG.info("<-- " + protocol(response.protocol()) + ' ' + response.code() + ' '
-                            + response.message() + " (" + tookMs + "ms"
-                            + (!logHeaders ? ", " + responseBody.contentLength() + "-byte body" : "") + ')');
-
-                    if (logHeaders) {
-                        Headers headers = response.headers();
-                        for (int i = 0, count = headers.size(); i < count; i++) {
-                            LOG.info(headers.name(i) + ": " + headers.value(i));
-                        }
-
-                        if (!logBody || !HttpEngine.hasBody(response) || "websocket".equals(headers.get("Upgrade"))) {
-                            LOG.info("<-- END HTTP");
-                        } else if (bodyEncoded(response.headers())) {
-                            LOG.info("<-- END HTTP (encoded body omitted)");
-                        } else {
-                            BufferedSource source = responseBody.source();
-                            source.request(Long.MAX_VALUE); // Buffer the entire body.
-                            Buffer buffer = source.buffer();
-
-                            Charset charset = UTF8;
-                            MediaType contentType = responseBody.contentType();
-                            if (contentType != null) {
-                                charset = contentType.charset(UTF8);
-                            }
-
-                            if (responseBody.contentLength() != 0) {
-                                LOG.info("");
-                                LOG.info(buffer.clone().readString(charset));
-                            }
-
-                            LOG.info("<-- END HTTP (" + buffer.size() + "-byte body)");
-                        }
-                    }
-
-                    return response;
-                }
-
-                private boolean bodyEncoded(Headers headers) {
-                    String contentEncoding = headers.get("Content-Encoding");
-                    return contentEncoding != null && !contentEncoding.equalsIgnoreCase("identity");
-                }
-
-                private String protocol(Protocol protocol) {
-                    return protocol == Protocol.HTTP_1_0 ? "HTTP/1.0" : "HTTP/1.1";
-                }
-
-
-            };
+            for (Interceptor interceptor : getHttpClient().interceptors()) {
+                if (interceptor instanceof LogInterceptor)
+                    return;
+            }
+            Interceptor hli = new LogInterceptor();
             getHttpClient().interceptors().add(hli);
         }
     }
@@ -225,4 +99,135 @@ public abstract class LoggingApiClient extends NamespacedApiClient {
         return kmf.getKeyManagers();
     }
 
+    private class LogInterceptor implements Interceptor {
+                /*
+                 Source from com.squareup.okhttp:logging-interceptor:2.7.5 , Apache 2 License.
+
+                 Modified to ignore "Upgrade: Websocket" responses.
+                 */
+
+        private final Charset UTF8 = Charset.forName("UTF-8");
+
+        private volatile HttpLoggingInterceptor.Level level = HttpLoggingInterceptor.Level.BODY;
+
+
+        @Override public Response intercept(Chain chain) throws IOException {
+            HttpLoggingInterceptor.Level level = this.level;
+
+            Request request = chain.request();
+            if (level == HttpLoggingInterceptor.Level.NONE) {
+                return chain.proceed(request);
+            }
+
+            boolean logBody = level == HttpLoggingInterceptor.Level.BODY;
+            boolean logHeaders = logBody || level == HttpLoggingInterceptor.Level.HEADERS;
+
+            RequestBody requestBody = request.body();
+            boolean hasRequestBody = requestBody != null;
+
+            Connection connection = chain.connection();
+            Protocol protocol = connection != null ? connection.getProtocol() : Protocol.HTTP_1_1;
+            String requestStartMessage =
+                    "--> " + request.method() + ' ' + request.httpUrl() + ' ' + protocol(protocol);
+            if (!logHeaders && hasRequestBody) {
+                requestStartMessage += " (" + requestBody.contentLength() + "-byte body)";
+            }
+            LOG.info(requestStartMessage);
+
+            if (logHeaders) {
+                if (hasRequestBody) {
+                    // Request body headers are only present when installed as a network interceptor. Force
+                    // them to be included (when available) so there values are known.
+                    if (requestBody.contentType() != null) {
+                        LOG.info("Content-Type: " + requestBody.contentType());
+                    }
+                    if (requestBody.contentLength() != -1) {
+                        LOG.info("Content-Length: " + requestBody.contentLength());
+                    }
+                }
+
+                Headers headers = request.headers();
+                for (int i = 0, count = headers.size(); i < count; i++) {
+                    String name = headers.name(i);
+                    // Skip headers from the request body as they are explicitly logged above.
+                    if (!"Content-Type".equalsIgnoreCase(name) && !"Content-Length".equalsIgnoreCase(name)) {
+                        LOG.info(name + ": " + headers.value(i));
+                    }
+                }
+
+                if (!logBody || !hasRequestBody) {
+                    LOG.info("--> END " + request.method());
+                } else if (bodyEncoded(request.headers())) {
+                    LOG.info("--> END " + request.method() + " (encoded body omitted)");
+                } else {
+                    Buffer buffer = new Buffer();
+                    requestBody.writeTo(buffer);
+
+                    Charset charset = UTF8;
+                    MediaType contentType = requestBody.contentType();
+                    if (contentType != null) {
+                        contentType.charset(UTF8);
+                    }
+
+                    LOG.info("");
+                    LOG.info(buffer.readString(charset));
+
+                    LOG.info("--> END " + request.method()
+                            + " (" + requestBody.contentLength() + "-byte body)");
+                }
+            }
+
+            long startNs = System.nanoTime();
+            Response response = chain.proceed(request);
+            long tookMs = TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - startNs);
+
+            ResponseBody responseBody = response.body();
+            LOG.info("<-- " + protocol(response.protocol()) + ' ' + response.code() + ' '
+                    + response.message() + " (" + tookMs + "ms"
+                    + (!logHeaders ? ", " + responseBody.contentLength() + "-byte body" : "") + ')');
+
+            if (logHeaders) {
+                Headers headers = response.headers();
+                for (int i = 0, count = headers.size(); i < count; i++) {
+                    LOG.info(headers.name(i) + ": " + headers.value(i));
+                }
+
+                if (!logBody || !HttpEngine.hasBody(response) || "websocket".equals(headers.get("Upgrade"))) {
+                    LOG.info("<-- END HTTP");
+                } else if (bodyEncoded(response.headers())) {
+                    LOG.info("<-- END HTTP (encoded body omitted)");
+                } else {
+                    BufferedSource source = responseBody.source();
+                    source.request(Long.MAX_VALUE); // Buffer the entire body.
+                    Buffer buffer = source.buffer();
+
+                    Charset charset = UTF8;
+                    MediaType contentType = responseBody.contentType();
+                    if (contentType != null) {
+                        charset = contentType.charset(UTF8);
+                    }
+
+                    if (responseBody.contentLength() != 0) {
+                        LOG.info("");
+                        LOG.info(buffer.clone().readString(charset));
+                    }
+
+                    LOG.info("<-- END HTTP (" + buffer.size() + "-byte body)");
+                }
+            }
+
+            return response;
+        }
+
+        private boolean bodyEncoded(Headers headers) {
+            String contentEncoding = headers.get("Content-Encoding");
+            return contentEncoding != null && !contentEncoding.equalsIgnoreCase("identity");
+        }
+
+        private String protocol(Protocol protocol) {
+            return protocol == Protocol.HTTP_1_0 ? "HTTP/1.0" : "HTTP/1.1";
+        }
+
+
+    }
 }
